67b4a752c79c31763fdd5250920be2ab4126eb3e
sushi-chaaaan <mail@sushichan.live>
2023-07-21 13:03:55 +0900

    stop use auth() and use currentUser()

--- app/components/topPage.tsx
+++ app/components/topPage.tsx
@@ -1,10 +1,10 @@
-import { auth } from '@clerk/nextjs'
+import { currentUser } from '@clerk/nextjs'
 
 import NowPlaying from '@/app/components/nowPlaying'
 import SpotifySigninButton from '@/app/components/spotifySigninButton'
 import TopItem from '@/app/components/topItem'
 export default async function TopPage() {
-  const { user } = auth()
+  const user = await currentUser()
   console.log(user)
 
   return (
