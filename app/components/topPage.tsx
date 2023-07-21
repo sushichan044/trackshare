bb984d1002d4ff0f7dc68abd44e04aa0054da2f2
sushi-chaaaan <mail@sushichan.live>
2023-07-21 12:24:21 +0900

    split ui into component

---
+++ app/components/topPage.tsx
@@ -0,0 +1,25 @@
+import { auth } from '@clerk/nextjs'
+
+import NowPlaying from '@/app/components/nowPlaying'
+import SpotifySigninButton from '@/app/components/spotifySigninButton'
+import TopItem from '@/app/components/topItem'
+export default async function TopPage() {
+  const { user } = auth()
+  console.log(user)
+
+  return (
+    <>
+      {user ? (
+        <>
+          <NowPlaying />
+          <h2>Your Top Items</h2>
+          <TopItem />
+        </>
+      ) : (
+        <div className="flex mt-8">
+          <SpotifySigninButton />
+        </div>
+      )}
+    </>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,10 @@
-import { auth } from '@clerk/nextjs'
-
-import NowPlaying from '@/app/components/nowPlaying'
-import SpotifySigninButton from '@/app/components/spotifySigninButton'
-import TopItem from '@/app/components/topItem'
+import TopPage from '@/app/components/topPage'
 import MainContainer from '@/components/common/mainContainer'
 export default async function Home() {
-  const { user } = auth()
-  console.log(user)
-
   return (
     <MainContainer>
       <h1 className="text-4xl text-center">Share Track</h1>
-      {user ? (
-        <>
-          <NowPlaying />
-          <h2>Your Top Items</h2>
-          <TopItem />
-        </>
-      ) : (
-        <div className="flex mt-8">
-          <SpotifySigninButton />
-        </div>
-      )}
+      <TopPage />
     </MainContainer>
   )
 }
